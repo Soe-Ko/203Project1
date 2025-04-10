3ace5cfe767c9c6b08ad4b11d76207e944e38f91
Char <ctgo9725@gmail.com>
2025-04-10 13:55:11 -0700

    Added psudocode for main file.
    Signed-off-by: Char <ctgo9725@gmail.com>

--- Project1.java
+++ Project1.java
@@ -1,5 +1,45 @@
 public class Project1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // for the number of lines in our file, do:
+            // init an integer to store the first number
+            // init a char to store the operator
+            // init an integer to store the second number
+
+            // use a switch case on the variable storing the operator
+                // case +:
+                    // call the add method
+                // case *:
+                    // call the multiplication method
+                // case ^:
+                    // call the exp method
+
     }
+
+    // create the private add method
+    /**
+     * Adds two integers.
+     *
+     * @param numA An integer to be added
+     * @param numB An integer to be added
+     * @return The integer result of adding numA and numB
+     */
+
+    // create the private multiplitication method
+    /**
+     * Multiplies two integers.
+     *
+     * @param numA An integer to be multiplied
+     * @param numB An integer to be multiplied
+     * @return The integer result of multiplying numA and numB
+     */
+
+    // create the private exponentiation method
+    /**
+     * Raises an integer to a specified power.
+     *
+     * @param mum An integer to be multiplied
+     * @param exp The number of times to multiply *num* by itself, represented by an integer
+     * @return The integer result of raising *num* to the specified power
+     */
 }
